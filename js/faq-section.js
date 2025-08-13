5ff0c2e9a959867ff33c0e0f23cceefeb7b759be
espinozaje <jeisson12aaron@gmail.com>
2025-08-12 23:02:45 -0500

    cambios v2

--- js/faq-section.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.faq-question').forEach(button => {
-  button.addEventListener('click', () => {
-    const faqItem = button.parentElement;
-    faqItem.classList.toggle('active');
-  });
-});
