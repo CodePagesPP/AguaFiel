1279d0c6f1524aa743629017c67152600bf819df
espinozaje <jeisson12aaron@gmail.com>
2025-09-01 20:20:39 -0500

    slider-mobile

--- js/slider-portada.js
+++ js/slider-portada.js
@@ -1,6 +1,12 @@
 const images = document.querySelectorAll(".hero-image");
 let currentIndex = 0;
 
+if (window.innerWidth <= 768) {
+  images[0].src = "./assets/portada-mobile-2.png";
+  images[1].src = "./assets/portada-mobile-3.png";
+  images[2].src = "./assets/portada-mobile-1.png";
+}
+
 function changeImage() {
   images[currentIndex].classList.remove("active");
   currentIndex = (currentIndex + 1) % images.length;
